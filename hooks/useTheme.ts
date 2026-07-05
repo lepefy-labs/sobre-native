e5613e9b931724fe5fa2352cd5435137132094ca
robertinsmartinvestor-gif <robertin.smartinvestor@gmail.com>
2026-07-05 21:31:41 +0200

    Delete hooks/useTheme.ts

--- hooks/useTheme.ts
+++
@@ -1,7 +0,0 @@
-import { useColorScheme } from 'react-native'
-import { lightColors, darkColors, type ThemeColors } from '@/constants/theme'
-
-export function useTheme(): ThemeColors {
-  const scheme = useColorScheme()
-  return scheme === 'dark' ? darkColors : lightColors
-}
